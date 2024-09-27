0f5386d1de655c7cc484a24089912e6c41540b37
doggydry <nestor.sernavaquero@iesmiguelherrero.com>
2024-09-27 12:32:37 +0200

    Subiendo ejercicios 1 y 2

---
+++ JavaScript/EJE1/index.js
@@ -0,0 +1,2 @@
+let persona = { nombre: 'Nestor', apellidos: 'Serna', FechaNAC: 2005 , edad: 19, }
+alert("\"" + persona.nombre + "\"" + persona.apellidos + "\"" + persona.edad + "\"" + persona.FechaNAC );
