20d33d81f8986a69e57e80662acfc17b211a7924
doggydry <153943833+doggydry@users.noreply.github.com>
2024-10-27 21:43:59 +0100

    Delete REFUERZOS/ANAGRAMAS directory
    ELIMINAR

--- REFUERZOS/ANAGRAMAS/scripts.js
+++
@@ -1,9 +0,0 @@
-function comprobarAnagramas(palabra1, palabra2) {
-    anagrama = false;
-    if (palabra1 === palabra2) {
-        console.log(` ${palabra1} y ${palabra2} NO son anagramas`);
-    }
-
-
-    }
-comprobarAnagramas('Casca', 'Cascada');
